154e8f2762eb47d64cd97be216503a345b415274
Kelebra <ricardokelebra@gmail.com>
2020-10-07 18:32:54 -0500

    repaso js

---
+++ 02_Kata_JS/repaso/main.js
@@ -0,0 +1,41 @@
+// var nombre = valor 
+
+// valores de en js 
+// --- >number o numerico 
+var numero1 = 10;
+// ---> string o texto
+var nombre = 'Ricardo';
+// ---> booleano o bool
+var verdadero = true // false 
+// var objecto = {
+
+// }
+
+// var arreglo = []
+
+var num1 = 5;
+var num2 = 5;
+
+// salidas o outputs
+// alert(nombre)
+console.log(num2 + num1)
+
+var apellido = 'Basurto'
+
+console.log(nombre + ' ' + apellido)
+
+// alert(nombre)
+// console.log('Hola comoe estas?')
+
+// entradas o inputs
+var saludo = 'Hola Como estas?'
+var  pregunta = prompt('Ingresa tu nombre');
+console.log(saludo + ' ' + pregunta)
+console.log(typeof(pregunta))
+
+
+var preguntaNum = prompt('Ingresa un numero');
+var numerReal = Number(preguntaNum)
+
+console.log(typeof(preguntaNum))
+console.log(typeof(numerReal))
