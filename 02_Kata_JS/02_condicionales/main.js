eb2eb2e64ff857e5810815bdd0c9dae32092a79d
Kelebra <ricardokelebra@gmail.com>
2020-10-06 21:57:08 -0500

    Condicionales

--- 02_Kata_JS/02_condicionales/main.js
+++ 02_Kata_JS/02_condicionales/main.js
@@ -38,9 +38,9 @@ if (numero !==  10) {
 // 2: par
 // Que pasa si el usuario pone letra x error 
 
-var preguntaNumero = prompt('Ingresa un numero');
+// var preguntaNumero = prompt('Ingresa un numero');
 
-var numeroVerdadero = Number(preguntaNumero);
+// var numeroVerdadero = Number(preguntaNumero);
 
 if (numeroVerdadero % 2 === 0 ) {
     console.log(numeroVerdadero + ': es par')
@@ -50,8 +50,36 @@ if (numeroVerdadero % 2 === 0 ) {
     console.error('No es un numero')
 }
 
+
+// Operdadores Logicos
+// &&  --> and
+// ||  -> or
+
+// es menor -> no puedes conducir
+// si tienes 16 o 17 -> puedes sacar el permiso
+// si eeres mayo de edad -> puedes sacar el permiso
+
+
+var edadPregunta = prompt('Que edad tienes');
+
+var edadNumero = Number(edadPregunta);
+
+
+if (edadNumero >= 18 && edadNumero <= 80) {
+    console.log('Puedes conducir :)')
+} else if (edadNumero === 16 || edadNumero === 17) {
+    console.log('Puedes sacar tu permiso para conducir chaval :)')
+} else if (edadNumero <= 15 || edadNumero > 80 ) {
+    console.warn('No puedes conducir por tu seguridad')
+} else {
+    console.error('No es una edad');
+}
+
 //-------- Challenge 2 --------
 // Juego de piedra papel o tijera ✂️ ⛰ 📃
 // Preguntar a 2 usuarios que opcion eleguir
 // Mostrar en consola que jugador gano
+
+
+
 
