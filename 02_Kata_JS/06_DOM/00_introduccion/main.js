8939def21146d8d0b12c46df71c06d6ca74d35dc
Mikee Ramirez <miguel.ramirez@nowports.com>
2020-10-15 21:24:22 -0500

    feat: adding intro to DOM
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- 02_Kata_JS/06_DOM/00_introduccion/main.js
+++ 02_Kata_JS/06_DOM/00_introduccion/main.js
@@ -2,7 +2,7 @@ function getName() {
   console.log(document.getElementById("myText").value);
   var firstName = document.getElementById("myText").value;
   document.getElementById("demo").innerHTML = firstName;
-}
+};
 
 function suma() {
   var num1 = Number(document.getElementById('num1').value);
@@ -10,4 +10,4 @@ function suma() {
   var resultado = num1 + num2;
 
   return document.getElementById("resultado").innerHTML = resultado;
-}
+};
