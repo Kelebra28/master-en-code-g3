4c21146d4993ed4e05ffdaf356d15068601ca3b3
Kelebra <ricardokelebra@gmail.com>
2020-11-10 20:03:39 -0600

    ECMA

---
+++ 03_Kata_Computer_Science/02_ECMA6/main.js
@@ -0,0 +1,50 @@
+// Vanilla JS
+var x = 10;
+// console.log(x);
+x = 500;
+// console.log(x);
+var x = 'Hola';
+// console.log(x);
+
+//-------------------------------> ECMA6 <--------------------------------
+
+//*----- let 
+let y = 50;
+// console.log(y);
+
+  y = 100;
+// console.log(y);
+
+//*------ const
+const z = '500';
+// console.log(z);
+// z = '800';
+// console.log(z)
+
+//*------Spread Operator
+
+let frutas = ['sandia', 'melon', 'platano'];
+
+console.log(frutas);
+console.log(...frutas)
+console.log('sadia', 'melon', 'platano')
+
+//*-----  String Template
+
+let name = 'Ricardo';
+let saludo = 'hola';
+
+// Hola, ricardo, como estas?
+
+ console.log(saludo + ', ' + name + ', como estas?');
+
+ console.log(`${saludo}, ${name}, como estas?`);
+ console.log('🔥');
+
+
+ // *----- Arrow fuction o funciones de flecha
+
+ const suma = (a,b) => {
+        return a +b
+ }
+ suma(5, 10)
