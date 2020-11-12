c60815d631a1ed6c783a334e9564c2f0b284a007
Kelebra <ricardokelebra@gmail.com>
2020-11-11 21:12:45 -0600

    herencia

--- 03_Kata_Computer_Science/03_POO/main.js
+++ 03_Kata_Computer_Science/03_POO/main.js
@@ -8,27 +8,27 @@ class Animal  {
         this.color = color
         this.raza = raza
         this.tipo = tipo
-    }
+    };
     //Metodo 
     dormir() {
         return `Mi mascota llamdo ${this.nombre} duerme porque ya tiene ${this.edad}`
-    }
+    };
 
     comer(){
         return `${this.nombre} ayer se comio una galleta`
-    }
-}
+    };
+};
 
-const miMascota = new Animal('Misha', 3, 'cafe', 'cruza', 'perro')
-const miMascota2 = new Animal('Poppy', 5)
+const miMascota = new Animal('Misha', 3, 'cafe', 'cruza', 'perro');
+const miMascota2 = new Animal('Poppy', 5);
 
-console.log(miMascota)
-console.log(miMascota.dormir())
-console.log(miMascota2)
-console.log(miMascota2.dormir())
-console.log(miMascota.comer())
+console.log(miMascota);
+console.log(miMascota.dormir());
+console.log(miMascota2);
+console.log(miMascota2.dormir());
 
 // Crer una clase llamda Atomovil , y van a tener 3 propiedades, color, marca, deportivo
 // 2 metodos (los que ustedes quieran)
 
 // Crear 3 carros distintos,
+
---
+++ 03_Kata_Computer_Science/04_herencia/main.js
@@ -0,0 +1,33 @@
+// SuperClase / Clase padre
+class Mascota {
+     constructor(nombre, patas){
+
+        this.nombre = nombre
+        this.patas = patas
+     }
+
+     comer(){
+         return `${this.nombre} esta comiendo`
+     }     
+}
+
+//subclase /clase hija
+
+class Perrito extends Mascota {
+    constructor (nombre, patas, colorPelaje){
+        super(nombre, patas)
+        this.colorPelaje = colorPelaje
+    }
+
+    ladran(){
+        return 'woooooooowf'
+    }
+};
+
+
+
+const Perrito1 = new Perrito('Misha', 4, 'cafe');
+
+console.log(Perrito1)
+console.log(Perrito1.comer())
+console.log(Perrito1.ladran())
