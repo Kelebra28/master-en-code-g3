52e3a33adda7fd270ab5621959a8ee0488d05fd1
Mikee Ramirez <miguel.ramirez@nowports.com>
2020-11-24 21:16:20 -0600

    feat: adding dataset
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

---
+++ 03_Kata_Computer_Science/06_callbacks/Ejercicio/data.js
@@ -0,0 +1,27 @@
+const empleados = [
+  {
+    id: 1,
+    name: 'Mikee'
+  },
+  {
+    id: 2,
+    name: 'Pilar'
+  },
+  {
+    id: 3,
+    name: 'Charli'
+  }
+];
+
+const salarios = [
+  {
+    id: 1,
+    salario: 1500,
+  },
+  {
+    id:2,
+    salario:3000,
+  }
+]
+
+module.exports = { empleados, salarios };
