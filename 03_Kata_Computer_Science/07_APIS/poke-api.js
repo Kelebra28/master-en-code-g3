8540ee8c66b5eb98452be13529f6c18f68025909
Kelebra <ricardokelebra@gmail.com>
2020-12-01 20:55:46 -0600

    traformar peticion

--- 03_Kata_Computer_Science/07_APIS/poke-api.js
+++ 03_Kata_Computer_Science/07_APIS/poke-api.js
@@ -1,14 +1,36 @@
 const request = require('request');
 
-request.get('https://pokeapi.co/api/v2/pokemon/200', (error, response, body) =>{
-    if (response.statusCode === 200) {
-        const data = JSON.parse(body);
-        // console.log(data);
-        console.log(`${data.name} 🥰`);
+// request.get('https://pokeapi.co/api/v2/pokemon/200', (error, response, body) =>{
+//     if (response.statusCode === 200) {
+//         const data = JSON.parse(body);
+//         // console.log(data);
+//         console.log(`${data.name} 🥰`);
 
-    }else{
-        console.error(`🚫 Ocurrio un error en la peticion 🚫 ${error}`);
-    }
-});
+//     }else{
+//         console.error(`🚫 Ocurrio un error en la peticion 🚫 ${error}`);
+//     }
+// });
 
 
+const pedirPokemon = (pokemon) => {
+    request.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, (error, response, body) =>{
+        if (response.statusCode === 200) {
+            const data = JSON.parse(body);
+            // console.log(data);
+            console.log(`${data.name} 🥰`);
+
+        }else{
+            console.error(`🚫 Ocurrio un error en la peticion 🚫 ${error}`);
+        }
+    });
+}
+
+pedirPokemon('ditto');
+pedirPokemon(500)
+
+
+// Hacer una funcion que haga una petición 
+// (Ejemplo: peticionLibro(“i robot”);
+// Buscar un libro y traer el o los autores del primer libro
+
+// http://openlibrary.org/search.json?q=i+robot) 
