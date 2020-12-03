13894880f51c3ede1d4cea514fa27ceb364b454a
Kelebra <ricardokelebra@gmail.com>
2020-12-02 20:49:25 -0600

    put and delete

--- 03_Kata_Computer_Science/08_HTTP/client.js
+++ 03_Kata_Computer_Science/08_HTTP/client.js
@@ -5,16 +5,7 @@ const URL_BASE = 'https://goodreads-devf-aaron.herokuapp.com'
 // end-points
 
 // -> /api/v1/authors
-const getAllAuthors = () => {
-request(`${URL_BASE}/api/v1/authors`, (err, res, body) => {
-    if(res.statusCode === 200){
-        const data = JSON.parse(body);
-        console.log(data);
-    }else{
-        console.log(err);
-    }
-});
-}
+
 
 // getAllAuthors()
 
@@ -26,7 +17,7 @@ request(`${URL_BASE}/api/v1/authors`, (err, res, body) => {
 // UPDATE
 // DELETE
 
-
+// CREATE
 const createAuthor = (name, last_name, nacionalidad, biography, gender, age, isAlive) => {
     const URL_POST = `${URL_BASE}/api/v1/authors/`
 
@@ -53,5 +44,64 @@ const createAuthor = (name, last_name, nacionalidad, biography, gender, age, isA
 
 }
 
-// createAuthor("Juanito", "Churrito", "MX", "Tengo antojo de un churro", "M", 52, true)
+// createAuthor("Juanito Guitierrito", "Churrito", "MX", "Tengo antojo de un churro", "M", 100, true)
 
+//READ
+const getAllAuthors = () => {
+    request.get(`${URL_BASE}/api/v1/authors`, (err, res, body) => {
+        if(res.statusCode === 200){
+            const data = JSON.parse(body);
+            console.log(data);
+        }else{
+            console.log(err);
+        }
+    });
+    }
+
+    
+// UPDATE
+
+//13491
+const updateAuthor =  (name, last_name, nacionalidad, biography, gender, age, isAlive, id) => {
+    const URL_PUT = `${URL_BASE}/api/v1/authors/${id}/`
+
+    const jsonSend = {
+        "name": name,
+        "last_name": last_name,
+        "nacionalidad": nacionalidad,
+        "biography" : biography,
+        "gender": gender,
+        "age": age,
+        "is_alive" : isAlive
+    }
+
+    request.put(URL_PUT, {form: jsonSend}, (err, res, body) => {
+
+            console.log(res.statusCode)
+        if(res.statusCode === 200){
+            const updateAuthor = JSON.parse(body)
+            console.log(updateAuthor)
+        }else{
+            console.log(err)
+        }
+    })
+}
+
+// updateAuthor("Ricardo Cabio", "Porfavor funciona",  "MX", "Tengo antojo de un churro", "M", 100, true, 13491)
+
+//DELETE
+
+const deleteAuthor = (id) => {
+    const URL_DELETE = `${URL_BASE}/api/v1/authors/${id}/`
+
+    request.delete(URL_DELETE, (err, res, body) => {
+        console.log(res.statusCode)
+        if(res.statusCode === 204){
+            console.log('Se elimino el usuario correctamente')
+        }else{
+            console.log(err, 'No se puedo borrar el autor')
+        }
+    })
+}
+
+deleteAuthor(12759)
