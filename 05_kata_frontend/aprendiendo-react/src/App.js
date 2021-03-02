3a0b9dcd7aba7903b873a976dae2cc07a8d4dcc3
Mikee Ramirez <miguel.ramirez@nowports.com>
2021-03-01 21:57:45 -0600

    feat: adding class component
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- 05_kata_frontend/aprendiendo-react/src/App.js
+++ 05_kata_frontend/aprendiendo-react/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jo <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +19,5 @@ function App() {
       </header>
     </div>
   );
-}
 
 export default App;
--- 05_kata_frontend/aprendiendo-react/src/index.js
+++ 05_kata_frontend/aprendiendo-react/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppClassComponent from './AppClass';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppClassComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
