39a1ff0a96e504cbd0ea0c3444983ba64690c281
Mikee Ramirez <miguel.ramirez@nowports.com>
2021-03-08 21:58:52 -0600

    feat: adding routes to my project
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- 05_kata_frontend/aprendiendo-rutas-react/src/App.js
+++ 05_kata_frontend/aprendiendo-rutas-react/src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from 'react-router-dom'
+
+import Home from './Home';
+
+const NotFound = () => <h2>404 Not Found</h2>;
+
+function About() {
+  return <h2>About page</h2>;
+}
 
 function App() {
+  // Ejemplo de componente switch explicado a metodo JS
+  // const ruta = 'Home'
+  // switch (ruta) {
+  //   case 'Home':
+  //     console.log('hola')
+  //     break;
+  //   default:
+  //     console.log('no es home')
+  //     break;
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Si voy a usar react router dom, el primer componente que voy a a llamar va hacer <Router />
+    // Router me va a permitir detectar las URL's
+    <Router>
+  
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <a href="http:/page/about" target="_blank" rel="noopener noreferrer"> Afuera</a>
+            </li>
+          </ul>
+        </nav>
+        {/* Switch  me va permitir identicar las URL y pintar el componente adecuado */}
+        <Switch>
+          {/* Adentro de Switch siempre va ir la Route */}
+          {/* Creacion de Rutas por path */}
+            {/* Cuanto entren al path "/" deber de pintar el componente home */}
+          <Route exact path="/">
+            <Home/>
+          </Route>
+            {/* Cuanto entren al path "/404" deber de pintar el componente NotFound */}
+          <Route exact path="/page/about">
+            <About/>
+          </Route>
+          <Route exact path="/404">
+            <NotFound/>
+          </Route>
+          {/* Redirect si no existe la ruta adonde quiero ir, y este
+          seria mi default del switch y siempre va a el final*/}
+          <Redirect to="/404"/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
