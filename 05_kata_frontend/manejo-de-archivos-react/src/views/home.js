553ad06ce109e669df239929b8659a7e17355073
Mikee Ramirez <miguel.ramirez@nowports.com>
2021-03-22 19:43:15 -0600

    feat: adding assets, components and views
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

---
+++ 05_kata_frontend/manejo-de-archivos-react/src/views/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import logo from '../assets/img/logo.svg';
+
+
+const Home = () => {
+    return(
+        <Header logo={logo}/>
+    )
+}
+
+export default Home;
