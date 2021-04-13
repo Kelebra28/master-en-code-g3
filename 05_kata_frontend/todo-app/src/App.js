700b9131554068ec1965cc64a5cd83a0d66944b5
Mikee Ramirez <miguel.ramirez@nowports.com>
2021-04-12 21:47:14 -0500

    Feat: adding react router
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- 05_kata_frontend/todo-app/src/App.js
+++ 05_kata_frontend/todo-app/src/App.js
@@ -1,16 +1,36 @@
-// import HolaMundoClass from './components/HolaMundoClass';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import HolaMundoClass from './components/HolaMundoClass';
 import HolaMundo from './components/HolaMundo';
 
 import './App.css';
 
+const NotFound = () => {
+  return <h2> Not Found </h2>
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <HolaMundoClass /> */}
-        <HolaMundo texto="Hola soy un stateless component"/>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Switch>
+            <Route exact path="/">
+             <HolaMundo texto="Hola soy un stateless component"/>
+            </Route>
+            <Route exact path="/class" component={HolaMundoClass}/>
+            <Route exact path="/notFound" render={() => <NotFound/>} />
+            
+            <Redirect push to="/notFound" />
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
 }
 
