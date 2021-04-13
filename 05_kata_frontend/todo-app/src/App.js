655aad661948738886de898ca0477e448188f9d6
Mikee Ramirez <miguel.ramirez@nowports.com>
2021-04-12 21:31:36 -0500

    feat: adding hola mundo stateless
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- 05_kata_frontend/todo-app/src/App.js
+++ 05_kata_frontend/todo-app/src/App.js
@@ -1,11 +1,14 @@
-import HolaMundoClass from './components/HolaMundoClass';
+// import HolaMundoClass from './components/HolaMundoClass';
+import HolaMundo from './components/HolaMundo';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HolaMundoClass />
+        {/* <HolaMundoClass /> */}
+        <HolaMundo texto="Hola soy un stateless component"/>
       </header>
     </div>
   );
