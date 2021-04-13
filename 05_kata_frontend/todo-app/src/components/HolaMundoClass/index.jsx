0524e1effea52b571b80d151525d79e45dfc41c6
Mikee Ramirez <miguel.ramirez@nowports.com>
2021-04-12 20:59:57 -0500

    feat: adding holamundoclass component to project
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

---
+++ 05_kata_frontend/todo-app/src/components/HolaMundoClass/index.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class HolaMundoClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      texto: 'Hola a todos desde el estado',
+      edad: 30,
+    }
+  }
+
+  componentDidMount() {
+    console.log('Se monta el componente');
+    setTimeout(
+      () => {
+        this.setState(
+          {
+            texto: 'Se actualizo el componente.'
+          }
+        )
+      },
+      3000
+    )
+  }
+  
+
+  render() {
+    console.log('RENDER')
+    const { texto, edad } = this.state;
+    return (
+      <>
+        <h1> Componente Hola Mundo </h1>
+        {/* <h2> {this.state.texto} </h2>
+        <h2> {this.state.edad} </h2> */}
+        <h2> {texto} </h2>
+        <h2> {edad} </h2>
+      </>
+    );
+  }
+}
+
+export default HolaMundoClass;
