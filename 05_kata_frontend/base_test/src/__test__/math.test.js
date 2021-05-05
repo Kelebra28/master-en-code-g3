acf21c7fc6c60017a0b02b6d65e3f1c7c923f452
Mikee Ramirez <miguel.ramirez@nowports.com>
2021-05-04 20:18:41 -0500

    Feat: adding all test
    Signed-off-by: Mikee Ramirez <miguel.ramirez@nowports.com>

--- 05_kata_frontend/base_test/src/__test__/math.test.js
+++ 05_kata_frontend/base_test/src/__test__/math.test.js
@@ -1,5 +1,5 @@
 // const suma = require('../suma');
-import { resta, suma } from '../math';
+import { dividir, multiplicar, resta, suma } from '../math';
 
 describe('Probemos la funcion suma', () => {
   // suma
@@ -12,5 +12,23 @@ describe('Probemos la funcion resta', () => {
   // resta
   test('Vamos a restar 1 - 2 para que el resultado no sea 3', () => {
     expect(resta(1, 2)).not.toBe(3);
+  });
+});
+
+describe('Probemos la funcion dividir', () => {
+  // dividir
+  test('Vamos a dividir 30 / 3 para que el resultado no sea 9', () => {
+    expect(dividir(30, 3)).not.toBe(9);
+  });
+});
+
+describe('Probemos la funcion multiplicar', () => {
+  // multiplicar
+  test('Vamos a multiplar 3 * 10 para que el resultado no sea 30', () => {
+    expect(multiplicar(3, 10)).not.toBe(3);
+  });
+
+  test('Vamos a multiplar 3 * 10 para que el resultado no sea 30', () => {
+    expect(multiplicar(3, 10)).toBe(30);
   });
 });
--- 05_kata_frontend/base_test/src/math.js
+++ 05_kata_frontend/base_test/src/math.js
@@ -1,3 +1,7 @@
 export const suma = (a, b) => a + b;
 
 export const resta = (a, b) =>  a - b;
+
+export const dividir = (a, b) => a / b;
+
+export const multiplicar = (a, b) => a * b;
